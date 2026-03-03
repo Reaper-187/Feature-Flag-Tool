a23707f63763b1f19281102016d533595ac67313
Reaper-187 <Abdulkader_cheik@outlook.de>
2026-03-03 23:37:33 +0100

    refactor: convert backend to full ESM setup and fix Prisma integration - Replaced require() with ES module imports - Updated type imports to align with ESM configuration - Add @types/cors dependency for TypeScript support - Resolved ESM/CommonJS conflicts - Adjusted Prisma client setup - Ensured server and database start correctly with tsx

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,33 +1,38 @@
 import { Request, Response } from "express";
+import prisma from "./lib/prisma";
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
 
 if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
+  await import("dotenv/config");
 }
-const express = require("express");
-// const cors = require("cors");
+
 const app = express();
 
 app.set("trust proxy", 1);
 
-const PORT = process.env.PORT || 5000;
+const PORT: number = parseInt(process.env.PORT || "5000", 10); // Port wird als number conv
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
-// app.use(
-//   cors({
-//     origin: [FRONTEND_URL],
-//     credentials: true,
-//   }),
-// );
+app.use(
+  cors({
+    origin: FRONTEND_URL ? [FRONTEND_URL] : true, // Fallback zu true wenn nicht gesetzt
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Backend running");
 });
 
 async function startServer() {
   try {
+    await prisma.$connect();
     app.listen(PORT, "0.0.0.0", () => {
-      console.log(`Server running at: ${PORT}`);
+      console.log(`DB is connected ----- Server running at: ${PORT}`);
     });
   } catch (error) {
     console.error("Failed to connect to DB:", error);
