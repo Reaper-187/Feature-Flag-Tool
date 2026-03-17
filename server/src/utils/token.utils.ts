965f61a055144c579469ab249f0f26cf2217cb16
Reaper-187 <Abdulkader_cheik@outlook.de>
2026-03-17 22:38:18 +0100

    feat(auth): install and configure JWT token utility - install jsonwebtoken dependency for token-based authentication - create token utility for generating JWTs - implement generateToken function with configurable payload and expiration - integrate environment variables for JWT secret and expiration time - ensure type safety for JWT configuration in TypeScript

---
+++ server/src/utils/token.utils.ts
@@ -0,0 +1,21 @@
+import jwt, { SignOptions } from "jsonwebtoken";
+
+const jwt_secret = process.env.JWT_SECRET;
+const jwt_exp_in = process.env.JWT_EXP_IN || "1h";
+
+if (!jwt_secret) {
+  throw new Error("JWT_SECRET is not defined");
+}
+
+export const generateToken = (user_id: string): string => {
+  const payload = { user_id };
+
+  const options: SignOptions = {
+    expiresIn: jwt_exp_in as SignOptions["expiresIn"],
+  };
+  // Durch den Cast auf SignOptions["expiresIn"] sagst du TypeScript trust me dawg dieser Wert ist kompatibel.
+
+  const token = jwt.sign(payload, jwt_secret, options);
+
+  return token;
+};
