e01d33e368d2a1b8d517765599d8f9987a990a78
Reaper-187 <Abdulkader_cheik@outlook.de>
2026-03-30 15:41:18 +0200

    feat(auth): implement register UI and flip-based auth layout - add structured Register form UI with validation schema zod + react-hook-form - integrate password visibility toggle and criteria feedback - refactor AuthPage to support flip animation between register and login - extract auth state (mode) into AuthPage and pass via props - implement CSS-based 3D flip animation - fix layout issues (width, height, centering) for stable rendering - integrate background image into AuthPage

---
+++ client/src/pages/AuthPage.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { Register } from "./auth-pages/Register";
+import { Login } from "./auth-pages/Login";
+import BgImg from "../../public/ff-bg.png";
+import "./flip.css";
+
+export const AuthPage = () => {
+  const [mode, setMode] = useState<"login" | "register">("login");
+
+  return (
+    <div
+      className="flex min-h-screen items-center justify-center bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url(${BgImg})` }}
+    >
+      <div className="flip-container">
+        <div className={`flip-card ${mode === "login" ? "flipped" : ""}`}>
+          {/* FRONT */}
+          <div className="flip-face flip-front">
+            <Register onSwitch={() => setMode("login")} />
+          </div>
+
+          {/* BACK */}
+          <div className="flip-face flip-back">
+            <Login onSwitch={() => setMode("register")} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- client/src/pages/auth-pages/Register.tsx
+++ client/src/pages/auth-pages/Register.tsx
@@ -1,103 +1,180 @@
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
 import {
   Card,
-  CardContent,
+  CardDescription,
   CardHeader,
   CardTitle,
-  CardDescription,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import { registerSchema, type RegisterFormValues } from "@/types/types";
+import { registerFormSchema } from "@/types/types";
+// import { guestAccessHook } from "@/hooks/AuthHooks/useGuestAccess";
+// import { useRegister } from "@/hooks/AuthHooks/useRegister";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Eye, EyeOff, User } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+type FormRegister = z.infer<typeof registerFormSchema>;
 
 type Props = {
   onSwitch: () => void;
 };
 
 export const Register = ({ onSwitch }: Props) => {
+  const [showPassword, setShowPassword] = useState(false);
+
   const {
     register,
     handleSubmit,
-    formState: { errors, isSubmitting },
-  } = useForm<RegisterFormValues>({
-    resolver: zodResolver(registerSchema),
+    watch,
+    formState: { errors },
+  } = useForm<FormRegister>({
+    resolver: zodResolver(registerFormSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+    },
   });
 
-  const onSubmit = async (data: RegisterFormValues) => {
+  // const { mutate, isPending: registLoad } = useRegister();
+
+  // const { mutate: guestLogin, isPending: guestLoginLoad } = guestAccessHook();
+
+  const handleRegister = (data: FormRegister) => {
+    // mutate(data);
     console.log(data);
-    // später: API call
   };
 
+  // const noCheck = registLoad || guestLoginLoad;
+
+  const checkPasswordCriteria = (password: string = "") => {
+    return {
+      minLength: password.length > 7,
+      hasUppercase: /[A-Z]/.test(password),
+      hasLowercase: /[a-z]/.test(password),
+      hasNumber: /[0-9]/.test(password),
+      hasSpecial: /[^A-Za-z0-9]/.test(password),
+    };
+  };
+
+  const criteriaList = [
+    "Minumum characters 8",
+    "One uppercase charachter",
+    "One lowercase charachter",
+    "One number",
+    "One speacial charachter",
+  ];
+
+  const passwordChecks = checkPasswordCriteria(watch("password"));
+
   return (
-    <div className="flex min-h-screen items-center justify-cente">
+    <div className="w-full h-full flex items-center justify-center p-4 bg-cover">
       <Card className="w-full max-w-md">
         <CardHeader>
-          <CardTitle>Create account</CardTitle>
-          <CardDescription>
-            Enter your details to create a new account
+          <CardTitle className="flex justify-self-center text-2xl md:text-3xl lg:text-4xl">
+            Register
+          </CardTitle>
+          <CardDescription className="text-center">
+            Hey, Enter your details to create a Account
           </CardDescription>
         </CardHeader>
 
-        <CardContent>
-          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-            <div className="space-y-2">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" {...register("name")} />
-              {errors.name && (
-                <p className="text-sm text-red-500">{errors.name.message}</p>
-              )}
-            </div>
-
-            <div className="space-y-2">
-              <Label htmlFor="email">Email</Label>
-              <Input id="email" type="email" {...register("email")} />
-              {errors.email && (
-                <p className="text-sm text-red-500">{errors.email.message}</p>
-              )}
-            </div>
-
-            <div className="space-y-2">
-              <Label htmlFor="password">Password</Label>
-              <Input id="password" type="password" {...register("password")} />
-              {errors.password && (
-                <p className="text-sm text-red-500">
-                  {errors.password.message}
-                </p>
-              )}
-            </div>
-
-            <div className="space-y-2">
-              <Label htmlFor="confirmPassword">Confirm Password</Label>
+        <form
+          className="px-5 flex flex-col gap-6"
+          onSubmit={handleSubmit(handleRegister)}
+        >
+          <div className="space-y-5">
+            <div className="flex gap-4">
               <Input
-                id="confirmPassword"
-                type="password"
-                {...register("confirmPassword")}
+                className="text-red-400"
+                type="firstName"
+                placeholder="firstname"
+                {...register("firstName")}
               />
-              {errors.confirmPassword && (
-                <p className="text-sm text-red-500">
-                  {errors.confirmPassword.message}
-                </p>
+              {errors.firstName && (
+                <p className="text-red-300">{errors.firstName?.message}</p>
+              )}
+              <Input
+                className="text-red-400"
+                type="lastName"
+                placeholder="lastname"
+                {...register("lastName")}
+              />
+              {errors.lastName && (
+                <p className="text-red-300">{errors.lastName?.message}</p>
               )}
             </div>
-
-            <Button type="submit" className="w-full" disabled={isSubmitting}>
-              {isSubmitting ? "Creating account..." : "Register"}
-            </Button>
-
-            <div className="text-center text-sm">
-              Already have an account?{" "}
+            <Input
+              className="text-red-400"
+              type="email"
+              placeholder="email"
+              {...register("email")}
+            />
+            {errors.email && (
+              <p className="text-red-600">{errors.email.message}</p>
+            )}
+            <div className="relative">
+              <Input
+                {...register("password")}
+                placeholder="password"
+                type={showPassword ? "text" : "password"}
+              />
               <button
                 type="button"
-                onClick={onSwitch}
-                className="text-sm underline"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute right-3 top-1/2 transform -translate-y-1/2"
               >
-                Already have an account?
+                {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
               </button>
             </div>
-          </form>
-        </CardContent>
+            {criteriaList.map((criterion, index) => {
+              const isMet = Object.values(passwordChecks)[index];
+              return (
+                <div className={isMet ? "text-green-500" : "text-gray-500"}>
+                  <li className="ml-5">{criterion}</li>
+                </div>
+              );
+            })}
+            {errors.password && (
+              <p className="text-red-600">{errors.password.message}</p>
+            )}
+          </div>
+          <div className="w-full grid grid-cols-2 gap-4">
+            <Button /*disabled={noCheck ? true : false}*/ className="w-full">
+              {/* {noCheck ? "please wait..." : "Registration"} */}
+              {"Registration"}
+            </Button>
+            <Button type="button" className="w-full" onClick={onSwitch}>
+              Switch to Login
+              {/* <Link className="w-full" to={"/login"} >
+              </Link> */}
+            </Button>
+          </div>
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center ">
+              <span className="w-full border-t " />
+            </div>
+            <div className="relative flex justify-center text-xs uppercase">
+              <span className="bg-primary-foreground px-2 text-muted-foreground rounded-md">
+                Or continue with
+              </span>
+            </div>
+          </div>
+        </form>
+        <div className="grid grid-cols-2 gap-4 px-4 ">
+          <Button
+            className="w-full col-span-2 font-semibold"
+            // onClick={() => guestLogin()}
+            // disabled={noCheck ? true : false}
+          >
+            <User className="mr-2 h-4 w-4" />
+            {/* {guestLoginLoad ? "Guest-Access is creating..." : "Guest for Test"} */}
+            {"Guest for Test"}
+          </Button>
+        </div>
       </Card>
     </div>
   );
